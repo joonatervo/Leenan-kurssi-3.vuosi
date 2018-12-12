c2b7b0f7b9f149d56e22509092ee0e8ee2b9d3e2
joonatervo <35563455+joonatervo@users.noreply.github.com>
2018-12-12 12:00:22 +0200

    Ajax Harjoitukset

---
+++ ajax harjoitukset/demo5.php
@@ -0,0 +1,22 @@
+<?php 
+$host ="magnesium"; 
+$user = "16bjoonat"; 
+$pass = "salasana"; 
+$dbname = "db16bjoonat"; 
+
+try  //yritetään ottaa yhteys 
+{  
+    $yhteys = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass); //luo PDO-olion 
+}  
+catch(PDOException $e) // jos ei onnistu (poikkeus) 
+{  
+    echo $e->getMessage(); //antaa ilmoituksen siitä, missä virhe 
+} 
+
+$sql = "SELECT * FROM pelaajat"; 
+$vastaus = $yhteys->prepare($sql); 
+$vastaus->execute(); 
+$vastaukset = $vastaus->fetchAll(PDO::FETCH_ASSOC); 
+
+echo json_encode($vastaukset);
+?> 
